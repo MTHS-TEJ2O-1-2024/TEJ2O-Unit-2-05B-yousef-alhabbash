3009039e7141d97af9bda452e775869fceb1ce61
yousef-alhabbash <yousef.al-habbash@mths.ca>
2024-09-24 11:08:51 -0400

    Converted Temp to kelvin

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Yousef
+ * Created on: Sep 2024
+ * This program will show our current temperature.
 */
+let temprature = input.temperature()
 
-basic.showString('Hello, World!')
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    temprature = input.temperature()
+
+    temprature = temprature + 273.15 
+    temprature = Math.round(temprature)
+
+    basic.showString('The temprature is: ' + temprature.toString() + 'K.')
+    
+    basic.showNumber(temprature)
+    
+
+
+})
